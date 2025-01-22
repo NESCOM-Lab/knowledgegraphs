9b94025a15b1c62f69c96bfb706e5ac029c94ef7
Abhishek Kakolla <abhishekkakolla1@gmail.com>
2025-01-21 21:15:55 -0800

    Added frontend & testing with large files (err)

---
+++ frontend/kgraphs/src/Chat.tsx
@@ -0,0 +1,20 @@
+
+
+interface Props {
+    text: string;
+}
+
+function Chat({text}: Props) {
+
+    return <>
+    
+        <p>{text}
+            
+        </p>
+    </>;
+
+
+
+}
+
+export default Chat;
