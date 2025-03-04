60781acd193721861d7d3ff89b767f57a8866e69
Abhishek Kakolla <abhishekkakolla1@gmail.com>
2025-03-04 00:12:16 -0800

    reinit frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Knowledge Graphs</h1>
+      <h4>System: </h4>
+      <input></input>
+    </>
+  )
+}
+
+export default App
