26adafa82c6599dd089527d201831fac6b1720a8
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-05-05 03:19:31 -0700

    switch to ollama

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -9,6 +9,7 @@ from langchain_core.prompts import PromptTemplate
 import getpass
 from langchain_experimental.graph_transformers import LLMGraphTransformer
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Neo4jVector
 from langchain_community.document_loaders import PyPDFLoader
@@ -79,26 +80,6 @@ def chunk_document(pdf_path):
     print(str(len(processed_chunks)) + " chunks processed")
     return processed_chunks
 
-async def ingest_document(processed_chunks, embed, llm_transformer, graph):
-    # Convert processed chunks to Langchain Document for Neo4j db
-    docs = [
-        Document(
-            page_content=chunk['text'],
-            metadata=chunk['metadata']
-        )
-        for chunk in processed_chunks
-    ]
-
-    for doc in docs:
-        # generate embeddings for each doc
-        embedding = embed.embed_query(doc.page_content)
-        doc.metadata["embedding"] = embedding
-
-        graph_docs = await llm_transformer.aconvert_to_graph_documents([doc])
-        print(f"Processed chunk {doc.metadata['chunk_id']}:")
-        graph.add_graph_documents(graph_docs, include_source=True, baseEntityLabel=True)
-        print(f"Successfully added chunk {doc.metadata['chunk_id']} to Neo4j")
-
 # async def ingest_document(processed_chunks, embed, llm_transformer, graph):
 #     # Convert processed chunks to Langchain Document for Neo4j db
 #     docs = [
@@ -109,54 +90,90 @@ async def ingest_document(processed_chunks, embed, llm_transformer, graph):
 #         for chunk in processed_chunks
 #     ]
 
-#     # Function to process documents in batches
-#     async def process_batches(docs, batch_size=2, retry_delay=20, max_retries=5):
-#         for i in range(0, len(docs), batch_size):
-#             batch = docs[i : i + batch_size] # Use subset of documents
-#             retries = 0
+#     for doc in docs:
+#         # generate embeddings for each doc
+#         embedding = embed.embed_query(doc.page_content)
+#         doc.metadata["embedding"] = embedding
+
+#         graph_docs = await llm_transformer.aconvert_to_graph_documents([doc])
+#         print(f"Processed chunk {doc.metadata['chunk_id']}:")
+#         graph.add_graph_documents(graph_docs, include_source=True, baseEntityLabel=True)
+#         print(f"Successfully added chunk {doc.metadata['chunk_id']} to Neo4j")
+
+async def ingest_document(processed_chunks, embed, llm_transformer, graph):
+    # Convert processed chunks to Langchain Document for Neo4j db
+    docs = [
+        Document(
+            page_content=chunk['text'],
+            metadata=chunk['metadata']
+        )
+        for chunk in processed_chunks
+    ]
+
+    # Function to process documents in batches
+    async def process_batches(docs, batch_size=2, retry_delay=20, max_retries=5):
+        # print("aconverting to graph documents")
+        # graph_docs = await llm_transformer.aconvert_to_graph_documents(docs)
+
+        # print("adding graph documents to DB")
+        # graph.add_graph_documents(graph_docs, include_source=True, baseEntityLabel=True)
+        # return
+        import time
+        start = time.time()
+        for i in range(0, len(docs), batch_size):
+            batch = docs[i : i + batch_size] # Use subset of documents
+            retries = 0
+            print("processing batch " + str(i // batch_size + 1))
             
-#             while retries < max_retries:
-#                 try:
-#                     # generate embeddings for each doc
-#                     for doc in batch:
-#                         embedding = embed.embed_query(doc.page_content)
-#                         doc.metadata["embedding"] = embedding
+            while retries < max_retries:
+                try:
+                    # generate embeddings for each doc
+                    for doc in batch:
+                        print("Creating embedding")
+                        embedding = embed.embed_query(doc.page_content)
+                        doc.metadata["embedding"] = embedding
+                        print("Embedding created")
 
-
-#                     graph_docs = await llm_transformer.aconvert_to_graph_documents(batch)
-#                     print(f"Processed batch {i // batch_size + 1}:")
+                    print("Converting to graph documents")
+                    graph_docs = await llm_transformer.aconvert_to_graph_documents(batch)
+                    print(f"Finished converting to graph documents {i // batch_size + 1}:")
                     
-#                     # Add to Neo4j
-#                     graph.add_graph_documents(graph_docs, include_source=True, baseEntityLabel=True)
-#                     print(f"Successfully added batch {i // batch_size + 1} to Neo4j")
-#                     break  # exit if retry works
+                    # Add to Neo4j
+                    print("adding to DB")
+                    graph.add_graph_documents(graph_docs, include_source=True, baseEntityLabel=True)
+                    print(f"Successfully added batch {i // batch_size + 1} to Neo4j DB")
+                    break  # exit if retry works
                 
-#                 except Exception as e:
-#                     if "rate_limit_exceeded" in str(e):
-#                         retries += 1
-#                         print(f"Rate limit hit. Retrying batch {i // batch_size + 1} in {retry_delay} seconds... (Attempt {retries}/{max_retries})")
-#                         await asyncio.sleep(retry_delay)
-#                     else:
-#                         raise  # other error
+                except Exception as e:
+                    if "rate_limit_exceeded" in str(e):
+                        retries += 1
+                        print(f"Rate limit hit. Retrying batch {i // batch_size + 1} in {retry_delay} seconds... (Attempt {retries}/{max_retries})")
+                        await asyncio.sleep(retry_delay)
+                    else:
+                        raise  # other error
+                
+        end = time.time()
+        print("Time taken: ", end - start)
 
-#     # Run batching process
-#     await process_batches(docs, batch_size=2)
+    # Run batching process
+    await process_batches(docs, batch_size=3)
 
 # returns llm_tranformer, embedding model, and vector_retriever
 def load_llm_transformer():
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo") # ChatOpenAI version
-    additional_instructions = """
-    When creating entities, add a "document_id" property to each node and set it to the document's unique ID.
-    For example, if the document ID is "doc123", each created node should include document_id: "doc123".
-    Query example: 
-    CREATE (n:NodeLabel) 
-    SET n.document_id = "doc123" 
-    RETURN n
-    """
-    llm_transformer = LLMGraphTransformer(llm=llm, additional_instructions=additional_instructions, ignore_tool_usage=True)
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-4.1-nano") # ChatOpenAI version
+    llm = ChatOllama(model="llama3.2:latest", format='json') # Ollama
+    # additional_instructions = """
+    # When creating entities, add a "document_id" property to each node and set it to the document's unique ID.
+    # For example, if the document ID is "doc123", each created node should include document_id: "doc123".
+    # Query example: 
+    # CREATE (n:NodeLabel) 
+    # SET n.document_id = "doc123" 
+    # RETURN n
+    # """
+    llm_transformer = LLMGraphTransformer(llm=llm)
 
     # Embeddings for later search queries
-    embed = OpenAIEmbeddings(model="text-embedding-ada-002")
+    embed = OpenAIEmbeddings(model="text-embedding-3-small")
     vector_index = Neo4jVector.from_existing_graph(
         embedding=embed,
         search_type="vector",
@@ -192,7 +209,7 @@ async def main():
     # Initialize openai
     print("Initializing OpenAI")
     api_key = os.getenv("OPENAI_API_KEY")
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-4.1-nano")
     llm_transformer, embed, vector_retriever = load_llm_transformer()
     
     
