2ba7e682aa4cc28b74ce4dd8619db6c2a6bdf038
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-05-19 06:40:56 -0400

    generate responses for each source w/ comparison agent

--- pipeline/comparison_agent.py
+++ pipeline/comparison_agent.py
@@ -1,11 +1,14 @@
 from typing import Dict, Any
+import openai
 
 class ComparisonAgent():
     def __init__(self, streamlit_obj):
         self.st = streamlit_obj
         self.prompt = """
-        Answer the question based on the given source and its context. Do not hallucinate or make up information.
-        If there is not enough information to answer the query based on the context, say so and explain what information you are missing.s[j]
+        Answer the question using only the information from the provided source.
+        Do not add or infer beyond what is explicitly stated.
+        If the source lacks enough information to answer, say so and specify what’s missing.
+        Begin your response with: 'Based on the paper "Paper name" by "Author",...
         """
         pass
         
@@ -17,11 +20,39 @@ class ComparisonAgent():
         for source, chunk in sources.items():
             self.st.write("Responding for " + source)
             full_query = f"""
-            Detailed Information: {chunk[0].page_content}
+            Source: {source} 
+            Source's Information: {chunk[0].page_content}
             Question: {query}
             """
-            self.st.write(full_query)
+            
+            # original chunk information 
+            with self.st.expander("See original information"):
+                self.st.write(chunk[0].page_content)
+
+
+
+            # add user's question
+            self.st.session_state.messages.append({"role": "user", "content": full_query})
+
+            # display LLM's response with streaming
+            response = ""
+            with self.st.chat_message("assistant"):
+                stream = openai.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[{"role": "system", "content": self.prompt}] + [
+                        {"role": msg["role"], "content": msg["content"]}
+                        for msg in self.st.session_state.messages
+                    ],
+                    stream=True
+                )
+                response = self.st.write_stream(stream)
+
+            # remove user's full query (w/ detailed info) and add just the question
+            self.st.session_state.messages.pop()
+            self.st.session_state.messages.append({"role": "user", "content": query})
+
+            # add llm's response to chat history
+            self.st.session_state.messages.append({"role": "assistant", "content": response})
         return  
         
-
 
